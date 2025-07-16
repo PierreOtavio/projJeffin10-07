3c49e41a7e93fe675c8a320feb98b0cec1ec815c
PierreOtavio <167585576+PierreOtavio@users.noreply.github.com>
2025-07-16 16:16:48 -0300

    comprimed project

--- interfaces/IBuscarOuCriarProduto.cs
+++
@@ -1,9 +0,0 @@
-﻿using WindowsFormsApp2.classes;
-
-namespace WindowsFormsApp2.interfaces
-{
-    public interface IBuscarOuCriarProduto
-    {
-        Produto BuscarOuCriarProduto(string descricao, string codigo);
-    }
-}
